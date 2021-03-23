94a20f6e77ff334a17f0e9d48cd00ace5e1ef3d6
190201099 <190201099@kocaeli.edu.tr>
2021-03-24 01:24:13 +0300

    GozlukluSirin moves one tile instead of two when there is a wall ahead

--- src/main/java/Game/KeyboardManager.java
+++ src/main/java/Game/KeyboardManager.java
@@ -2,6 +2,7 @@ package Game;
 import Game.Karakterler.Karakter;
 import Game.Karakterler.Oyuncu;
 
+import java.awt.*;
 import java.awt.event.*;
 
 public class KeyboardManager implements KeyListener{
@@ -18,36 +19,102 @@ public class KeyboardManager implements KeyListener{
     public void keyPressed(KeyEvent keyEvent) {
         int keyCode = keyEvent.getKeyCode();
         switch (keyCode) {
+
             case KeyEvent.VK_UP -> {
-                Oyuncu.sira--;
+                if(MenuState.ButtonID == 2){
+                    Oyuncu.sira--;
 
-                if((Oyuncu.sira >= 0 && Oyuncu.sira < 11) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
-                    Karakter.y -= 64;
-                else
-                    Oyuncu.sira++;;
+                    if((Oyuncu.sira >= 0 && Oyuncu.sira < 11) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
+                        Karakter.y -= 64;
+                    else
+                        Oyuncu.sira++;;
+                }else if(MenuState.ButtonID == 1){
+
+                    Oyuncu.sira--;
+                    if((Oyuncu.sira >= 0 && Oyuncu.sira < 11) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
+                        Karakter.y -= 64;
+                    else
+                        Oyuncu.sira++;;
+
+                    Oyuncu.sira--;
+                    if((Oyuncu.sira >= 0 && Oyuncu.sira < 11) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
+                        Karakter.y -= 64;
+                    else
+                        Oyuncu.sira++;;
+
+                }
             }
+
             case KeyEvent.VK_DOWN -> {
-                Oyuncu.sira++;
+                if(MenuState.ButtonID == 2){
+                    Oyuncu.sira++;
 
-                if((Oyuncu.sira >= 0 && Oyuncu.sira < 11) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
-                    Karakter.y += 64;
-                else
-                    Oyuncu.sira--;;
+                    if((Oyuncu.sira >= 0 && Oyuncu.sira < 11) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
+                        Karakter.y += 64;
+                    else
+                        Oyuncu.sira--;;
+                }else if(MenuState.ButtonID == 1){
+                    Oyuncu.sira++;
+                    if((Oyuncu.sira >= 0 && Oyuncu.sira < 11) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
+                        Karakter.y += 64;
+                    else
+                        Oyuncu.sira--;
+                    Oyuncu.sira++;
+                    if((Oyuncu.sira >= 0 && Oyuncu.sira < 11) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
+                        Karakter.y += 64;
+                    else
+                        Oyuncu.sira--;;
+                }
             }
+
             case KeyEvent.VK_LEFT -> {
-                Oyuncu.sutun--;
-
-                if((Oyuncu.sutun >=0 && Oyuncu.sutun < 13) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
-                    Karakter.x -= 64;
-                else
-                    Oyuncu.sutun++;
-            }
-            case KeyEvent.VK_RIGHT -> {
-                Oyuncu.sutun++;
-                if((Oyuncu.sutun >=0 && Oyuncu.sutun < 13) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
-                    Karakter.x += 64;
-                else
+                if(MenuState.ButtonID == 2){
                     Oyuncu.sutun--;
+
+                    if((Oyuncu.sutun >=0 && Oyuncu.sutun < 13) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
+                        Karakter.x -= 64;
+                    else
+                        Oyuncu.sutun++;
+                }
+                else if(MenuState.ButtonID == 1){
+
+                    Oyuncu.sutun--;
+                    if((Oyuncu.sutun >=0 && Oyuncu.sutun < 13) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
+                        Karakter.x -= 64;
+                    else
+                        Oyuncu.sutun++;
+
+                    Oyuncu.sutun--;
+                    if((Oyuncu.sutun >=0 && Oyuncu.sutun < 13) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
+                        Karakter.x -= 64;
+                    else
+                        Oyuncu.sutun++;
+
+                }
+            }
+
+            case KeyEvent.VK_RIGHT -> {
+                if(MenuState.ButtonID == 2){
+                    Oyuncu.sutun++;
+                    if((Oyuncu.sutun >=0 && Oyuncu.sutun < 13) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
+                        Karakter.x += 64;
+                    else
+                        Oyuncu.sutun--;
+                }
+                else if(MenuState.ButtonID == 1){
+                    Oyuncu.sutun++;
+                    if((Oyuncu.sutun >=0 && Oyuncu.sutun < 13) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
+                        Karakter.x += 64;
+                    else
+                        Oyuncu.sutun--;
+
+                    Oyuncu.sutun++;
+                    if((Oyuncu.sutun >=0 && Oyuncu.sutun < 13) && GameState.matrix[Oyuncu.sira][Oyuncu.sutun] == 1)
+                        Karakter.x += 64;
+                    else
+                        Oyuncu.sutun--;
+
+                }
             }
         }
     }
